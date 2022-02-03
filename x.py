338d85acc29ee968af2ca31ea2383678679b0858
ernestoc14 <descreed34@hotmail.com>
2022-02-03 12:22:19 -0500

    Add Python File

---
+++ x.py
@@ -0,0 +1,13 @@
+#Comentario
+#if 3 > 5:
+  #  print('Esto no de va a imprimir')
+
+x=5; y='Chanchito feliz'
+
+print(x, y)
+email='chanchofe23.com'
+print(email)
+inicio= 'Hola'
+final='Chao'
+
+print(inicio + final)
