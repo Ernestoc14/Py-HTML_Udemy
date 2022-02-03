2fa22c8ad88f0606e5e4245a4eba68edd4a2f657
ernestoc14 <descreed34@hotmail.com>
2022-02-03 15:17:27 -0500

    act

--- x.py
+++ x.py
@@ -2,12 +2,24 @@
 #if 3 > 5:
   #  print('Esto no de va a imprimir')
 
-x=5; y='Chanchito feliz'
+x=5
+y='Chanchito feliz'
+#print(x, y)
 
-print(x, y)
-email='chanchofe23.com'
-print(email)
-inicio= 'Hola'
+inicio= 'Hola '
 final='Chao'
+#print(inicio + final)
 
-print(inicio + final)
+lista=[0, 9, 3]
+lista2=lista.copy()
+#lista.clear()   #Borra toda la lista
+lista.append(6) #Agrega el elemento 6 a la lista
+
+#print(lista, lista2.count(3)) #cuenta cuantas veces se repite un elemento en array
+#print(len(lista)) #longitud de lista
+#lista.pop() #elimina el ultimo elemento de array
+#lista.remove('lazlo') #elimina elemento especifico
+
+lista.reverse()
+lista.sort()
+print(lista)
