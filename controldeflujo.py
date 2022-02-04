f72ed0509abae06536c9fe093d9c0ba7c3614eb3
ernestoc14 <descreed34@hotmail.com>
2022-02-04 17:52:57 -0500

    If Elif Else

---
+++ controldeflujo.py
@@ -0,0 +1,28 @@
+#if 2 < 5:
+    #print('2 es menor que 5')
+
+# a == b
+# a != b 
+# a <= >= b
+# a < > b
+
+"""a=6
+if 2 == 2:
+    print('2 es igual a 2')
+ 
+if 2 == 3:
+    print('2 es igual a 3')
+
+if a >= 3:
+    print('a es distinto a 3')"""
+
+
+if 2 > 5:
+    print('dos es menor a 5 en if')
+elif 2 == 5:
+    print('2 es menor a 5 en elif')
+elif 5 == 5:
+    print('2 es menor a 5 en seg elif')
+else:
+    print('esto es else')
+    
---
+++ ejercicios.py
@@ -0,0 +1,7 @@
+edad = input('Cuantos anios tienes: ')
+lista = ['hola','ernesto','lazlo','jean']
+
+if lista.count(edad) > 0:
+    print('El dato existe: ', edad)
+else:
+    print('El dato no existe: ', edad)
