71fe606144a38ad5567d8358c02fd9bd2c1feb21
ernestoc14 <descreed34@hotmail.com>
2022-02-15 11:02:18 -0500

    Funcion Recursividad

--- function.py
+++ function.py
@@ -1,6 +1,21 @@
 # funciones en Python\
 
+from ast import arg
+
+
 def miFuncion():
     print('Mi primer funcion')
 
 miFuncion()
+def impDato(nombre, apellido):
+    print('El nombre completo es:', nombre, apellido)
+
+impDato('Pondo',15)
+
+def recursion(i):
+    if(i <= 1):
+        return i
+    print(i)
+    recursion (i - 1)
+
+recursion(6)
